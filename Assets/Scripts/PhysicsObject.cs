4aade38268a40b8d4b4af7a48a9a5b49896dd84e
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2024-09-24 15:20:13 -0500

    assets imported

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshRenderer))]
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField]
+    private Material _awakeMaterial;
+    [SerializeField]
+    private Material _asleepMaterial;
+
+    private Rigidbody _rigidBody;
+    private MeshRenderer _meshRenderer;
+    private bool _wasSleeping = true;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMeshRenderer();
+    }
+
+    private void UpdateMeshRenderer()
+    {
+        if (_rigidBody.IsSleeping() && !_wasSleeping && _asleepMaterial != null)
+        {
+            _wasSleeping = true;
+            _meshRenderer.material = _asleepMaterial;
+        }
+        if (!_rigidBody.IsSleeping() && _wasSleeping && _awakeMaterial != null)
+        {
+            _wasSleeping = false;
+            _meshRenderer.material = _awakeMaterial;
+        }
+    }
+}
