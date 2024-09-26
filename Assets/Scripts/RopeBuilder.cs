ce3547d7b690c93d05bd9345aaa04200f549375b
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2024-09-26 14:07:35 -0500

    Rope Builder Implemented

---
+++ Assets/Scripts/RopeBuilder.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RopeBuilder : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_ropeObject;
+
+    [SerializeField]
+    private int m_sectionAmount;
+
+    [SerializeField]
+    private int m_sectionDistance;
+
+
+    private Vector3 newPosition;
+
+    private void Awake()
+    {
+        AddRope(m_sectionAmount);
+    }
+
+    private void AddRope(int ropeLength)
+    {
+        newPosition = new Vector3(transform.position.x, transform.position.y + m_sectionDistance, transform.position.z);
+
+        GameObject currentSection = Instantiate(m_ropeObject, transform.position, Quaternion.identity);
+
+        for (int i = 1; i < ropeLength; i++)
+        {
+            currentSection.transform.localPosition = transform.position;
+            GameObject nextSection = Instantiate(m_ropeObject, newPosition, currentSection.transform.rotation);
+            currentSection.GetComponent<SpringJoint>().connectedBody = nextSection.GetComponent<Rigidbody>();
+            currentSection = nextSection;
+        }
+    }
+}
